77d217739ae4d5eae4c13dfe1c73733f8634589a
Sanjay <ksanjayk@protonmail.com>
2023-05-31 15:29:16 +0530

    ledger struct changed to satisfy vdr

--- holder/utils/ledgers.ts
+++ holder/utils/ledgers.ts
@@ -1,10 +1,8 @@
 import { pool_transactions_bcovrin_test_genesis } from './txns';
+import type { IndyVdrPoolConfig } from '@aries-framework/indy-vdr';
 
-export const ledgers = [
-  {
-    id: `bcovrin-test-net`,
-    isProduction: false,
-    indyNamespace: 'bcovrin:test',
-    genesisTransactions: pool_transactions_bcovrin_test_genesis,
-  },
-];
+export const ledgers = {
+  isProduction: false,
+  indyNamespace: 'bcovrin:test',
+  genesisTransactions: pool_transactions_bcovrin_test_genesis,
+} satisfies IndyVdrPoolConfig;
